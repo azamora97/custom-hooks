03942a4fae400f87aed0a9936aaff9d628429ade
Andres Zamora <jzamoram@netcom.com.pa>
2024-05-13 09:58:12 -0600

    useTodos Agregado

---
+++ useTodos/todoReducer.js
@@ -0,0 +1,42 @@
+
+import PropTypes from 'prop-types' 
+
+export const todoReducer = ( initialState , action ) => {
+
+    switch(action.type) {
+        case '[TODO] Add Todo':
+                return [ ...initialState, action.payload ];
+            break;
+
+        case '[TODO] Remove Todo':
+                return initialState.filter( todo => todo.id !== action.payload );
+            break;
+
+        case '[TODO] Toggle Todo':
+            return initialState.map( (todo) => {
+                
+                if(todo.id === action.payload){
+                    return {
+                        ...todo,
+                        done: !todo.done
+                    }
+                }
+
+                return todo;
+            });
+        break;
+        
+        default:
+            return initialState;
+             break;
+    }
+}
+
+todoReducer.propType = {
+    initialState: PropTypes.array.isRequired,
+    action: PropTypes.string.isRequired
+}
+
+todoReducer.defaultProps = {
+    initialState: []
+}
